8bdad92fae9a3f5350d35fded4f7cadb02bf8ac5
shenlianhui <2976392462@qq.com>
2020-10-26 16:18:30 +0800

    搜索商品界面布局

---
+++ src/components/搜索商品界面.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+    </el-container>
+</template>
+<style lang="less" scoped>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
+</style>
