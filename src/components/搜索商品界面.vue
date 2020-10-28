83919f3562669fdcb9acb3cf57bb5727d1888c4b
shenlianhui <2976392462@qq.com>
2020-10-28 11:25:22 +0800

    28日提交

--- src/components/搜索商品界面.vue
+++ src/components/搜索商品界面.vue
@@ -2,11 +2,8 @@
   <div>
     <Top></Top>
   </div>
-  <!-- <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-    </el-container> -->
 </template>
+
 
 <script>
 import Top from "../components/top.vue";
@@ -23,17 +20,4 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.el-header,
-.el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
 </style>
